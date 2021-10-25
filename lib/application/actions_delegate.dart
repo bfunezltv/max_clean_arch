53b9dc4c75212cb5850878bdf9c0cd9b4628869b
Maxeem <jmax.posh@gmail.com>
2021-10-25 08:21:46 -0300

    chore: comment unused code

--- lib/application/actions_delegate.dart
+++ lib/application/actions_delegate.dart
@@ -1,5 +1,4 @@
 import 'package:max_clean_arch/core/product.domain/product.dart';
-import 'package:rxdart/src/utils/error_and_stacktrace.dart';
 
 abstract class ActionsDelegate {
   Stream<Product> get productOpenedStream; //
@@ -9,11 +8,11 @@ abstract class ActionsDelegate {
   Stream<void> get splashHiddenStream; //
   void onSplashHidden();
 
-  //
-  Stream<ErrorAndStackTrace> get loggedInErrorStream; //
-  void onLoggedInError(ErrorAndStackTrace error);
-
-  //
-  Stream<void> get loggedOutStream; //
-  void onLoggedOut();
+  // //
+// Stream<ErrorAndStackTrace> get loggedInErrorStream; //
+// void onLoggedInError(ErrorAndStackTrace error);
+//
+// //
+// Stream<void> get loggedOutStream; //
+// void onLoggedOut();
 }
